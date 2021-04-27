21ab7bfdc3082a14b8a557eb5e079d24b2da5d99
edsonpaulo <edsonpaulo24@gmail.com>
2021-04-27 18:20:40 +0100

    refactor: article new model using enums

--- src/app/models/article.js
+++ src/app/models/article.js
@@ -1,26 +1,28 @@
 "use strict";
 const mongoose = require("../../database");
 const mongoosePaginate = require("mongoose-paginate-v2");
-const { ArticleCategories } = require("../utils/constants");
+const { ArticleCategories, Shared } = require("../utils/constants");
 const Schema = mongoose.Schema;
 
 const Article = new Schema({
-  title: { type: String, required: true, trim: true },
-  body: { type: String, required: true },
-  image: { type: String, trim: true },
-  slug: { type: Strirng, trim: true, required: true, unique: true },
-  likes: { type: Number, default: 0 },
-  categories: [{ type: String, enum: Object.values(ArticleCategories) }],
-  comments: [
+  [Shared.Title]: { type: String, required: true, trim: true },
+  [Shared.Body]: { type: String, required: true },
+  [Shared.Image]: { type: String, trim: true },
+  [Shared.Slug]: { type: Strirng, trim: true, required: true, unique: true },
+  [Shared.Likes]: { type: Number, default: 0 },
+  [Shared.Categories]: [
+    { type: String, enum: Object.values(ArticleCategories) },
+  ],
+  [Shared.Comments]: [
     {
-      username: { type: String, trim: true },
-      email: { type: String, trim: true },
-      body: { type: String, trim: true },
-      createdAt: { type: Date, default: Date.now },
+      [Shared.Name]: { type: String, trim: true },
+      [Shared.Email]: { type: String, trim: true },
+      [Shared.Body]: { type: String, trim: true },
+      [Shared.CreatedAt]: { type: Date, default: Date.now },
     },
   ],
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  [Shared.CreatedAt]: { type: Date, default: Date.now },
+  [Shared.UpdatedAt]: { type: Date, default: Date.now },
 });
 
 Article.plugin(mongoosePaginate);
